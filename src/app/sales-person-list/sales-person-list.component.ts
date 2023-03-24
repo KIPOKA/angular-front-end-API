d31515e33db72336205ce8d561729765af42af1a
220825213 <220825213@tut4life.ac.za>
2023-03-24 15:19:01 +0200

    Implement the boostrap

--- src/app/sales-person-list/sales-person-list.component.ts
+++ src/app/sales-person-list/sales-person-list.component.ts
@@ -3,14 +3,14 @@ import { SalesPerson } from './sales-person';
 
 @Component({
   selector: 'app-sales-person-list',
-  templateUrl: './sales-person-list.component.html',
+  templateUrl: './sales-person-list.component.boostrap.html',
   styleUrls: ['./sales-person-list.component.css']
 })
 export class SalesPersonListComponent implements OnInit {
   //create an array of objects
   salesPersonList: SalesPerson[] = [
     new SalesPerson("AAnish","Kumar","anish@gmail.com", 3000),
-    new SalesPerson("Delhi","Angel","angel@gmail.com", 4000),
+    new SalesPerson("Delhi","Angel","angel@gmail.com", 6980),
     new SalesPerson("London","Bridge","london@gmail.com", 4500),
     new SalesPerson("Paris","Eiffel","eiffel@gmail.com", 8900)
   ];
